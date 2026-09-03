923684a82193331585acc315785dcf2f8f426d09
Dr. Hilquias S. Almeida <HilquiasAlmeida@users.noreply.github.com>
2026-09-03 00:18:59 -0300

    Add TelemetriaSerializer for Telemetria model

--- Computer-Engineering/Projects/Python/Systems-Hub/smart-containers/system/api/serializers.py
+++ Computer-Engineering/Projects/Python/Systems-Hub/smart-containers/system/api/serializers.py
@@ -1,1 +1,7 @@
+from rest_framework import serializers
+from .models import Container, Telemetria
 
+class TelemetriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Telemetria
+        fields = ['id', 'container', 'status_porta', 'latitude', 'longitude', 'temperatura', 'data_hora']
