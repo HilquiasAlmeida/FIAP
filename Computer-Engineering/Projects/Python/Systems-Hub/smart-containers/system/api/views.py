ca391ff3486f011e06d6d7870e70b945e0434ec0
Dr. Hilquias S. Almeida <HilquiasAlmeida@users.noreply.github.com>
2026-09-03 00:19:33 -0300

    Add TelemetriaCreateView for creating Telemetria records

--- Computer-Engineering/Projects/Python/Systems-Hub/smart-containers/system/api/views.py
+++ Computer-Engineering/Projects/Python/Systems-Hub/smart-containers/system/api/views.py
@@ -1,1 +1,7 @@
+from rest_framework import generics
+from .models import Telemetria
+from .serializers import TelemetriaSerializer
 
+class TelemetriaCreateView(generics.CreateAPIView):
+    queryset = Telemetria.objects.all()
+    serializer_class = TelemetriaSerializer
