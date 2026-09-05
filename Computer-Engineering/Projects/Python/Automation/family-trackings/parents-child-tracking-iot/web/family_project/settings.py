f3300c1d710ffd2ed6d65a90e0658cc033ca5ae3
Dr. Hilquias S. Almeida <HilquiasAlmeida@users.noreply.github.com>
2026-09-04 23:37:14 -0300

    Configure PostgreSQL database settings
    Added database configuration for PostgreSQL.

---
+++ Computer-Engineering/Projects/Python/Automation/family-trackings/parents-child-tracking-iot/web/family_project/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'family_db',         # Nome do seu banco de dados
+        'USER': 'postgres',          # Seu usuário do PostgreSQL
+        'PASSWORD': 'sua_senha',     # Sua senha do PostgreSQL
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
