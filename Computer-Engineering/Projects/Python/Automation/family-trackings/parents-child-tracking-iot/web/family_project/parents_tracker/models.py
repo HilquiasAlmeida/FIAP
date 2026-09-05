5d30911eebe8ffc197fa702072e68e3e92c0bdb5
Dr. Hilquias S. Almeida <HilquiasAlmeida@users.noreply.github.com>
2026-09-04 23:50:17 -0300

    Add ChildDevice model for tracking devices

--- Computer-Engineering/Projects/Python/Automation/family-trackings/parents-child-tracking-iot/web/family_project/parents_tracker/models.py
+++ Computer-Engineering/Projects/Python/Automation/family-trackings/parents-child-tracking-iot/web/family_project/parents_tracker/models.py
@@ -1,1 +1,14 @@
+from django.db import models
 
+class ChildDevice(models.Model):
+    name = models.CharField(max_length=100)
+    equipment = models.CharField(max_length=100)
+    serial_number = models.CharField(max_length=100, unique=True)
+    status = models.CharField(max_length=50, default="Online")
+    battery = models.CharField(max_length=10, default="100%")
+    latitude = models.CharField(max_length=50, default="-23.5505")
+    longitude = models.CharField(max_length=50, default="-46.6333")
+    location_name = models.CharField(max_length=200, default="Residência")
+
+    def __str__(self):
+        return f"{self.name} - {self.equipment}"
